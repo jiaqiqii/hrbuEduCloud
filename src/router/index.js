c5ffc4c1d23b1b764d0bbf67b3c5fc0d871441f0
jiaqiqii <qiqia826@163.com>
2022-05-24 20:49:06 +0800

    登录，学生管理部分页面布局

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+
+Vue.use(VueRouter)
+
+// 引入登陆组件
+import Login from "../views/Login"
+// 引入忘记密码
+import Forgetpassword from "../views/ForgetPassword"
+// 引入管理员首页
+import AdminIndex from "../views/AdminIndex"
+// 引入学生端首页
+import StudentIndex from "../views/StudentIndex"
+
+import Menus from "../views/Menus"
+
+import StuManage from "../views/StuManage"
+
+export default new VueRouter({
+    routes:[
+    {
+        path:"/",
+        name:"Login",
+        component:Login,
+    },
+    {
+        path:"/forgetpassword",
+        name:"Forgetpassword",
+        component:Forgetpassword,
+    },
+    {
+        path:"/admin",
+        name:"AdminIndex",
+        component:AdminIndex,
+    },
+    {
+        path:"/student",
+        name:"StudentIndex",
+        component:StudentIndex,
+    },
+    {
+        path:"/menus",
+        name: "Menus",
+        component: Menus,
+        children:[
+            {
+                path:"stumanage",
+                name: "StuManage",
+                component: StuManage,
+            }
+        ]
+    },
+]
+})
---
+++ src/views/StuManage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="stumanage">
+    <p>学生管理</p>
+    <el-row>
+      <el-button type="primary" size="medium">新增学生*</el-button>
+      <el-button type="primary" size="medium">批量导入</el-button>
+      <el-button size="medium" @click="resetPassword" :disabled="false"
+        >重置密码</el-button
+      >
+      <el-button size="medium" :disabled="true">结课</el-button>
+      <el-button size="medium" :disabled="true">激活</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"StuManage",
+
+}
+</script>
+
+<style lang="less" scoped>
+.stumanage {
+  margin-left: 20px;
+  width: 100%;
+  p {
+    color: #7a7f85;
+    line-height: 56px;
+  }
+}
+</style>
